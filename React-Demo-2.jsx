f5c53442ee456eabb7bebdc29881208beb8e3fd2
hartduenas08 <103306862+hartduenas08@users.noreply.github.com>
2024-01-22 00:13:22 +0800

    Update React-Demo-2.jsx

--- React-Demo-2.jsx
+++ React-Demo-2.jsx
@@ -1,27 +1,25 @@
 function Rainy(){
-  return(
-    <h1>Bring your umbrealla!</h1>
-  );
-}
-
-function RainOrShine(props){
-  const isRainy = props.isRainy;
-  if(isRainy){
     return(
-      <Rainy />
+        <h1>Bring your umbrella!</h1>
     );
-  }
-  return(
-    <h1>No rain today!</h1>
-  );
 }
 
-export default function App(){
-  return(
-    <div>
-      <RainOrShine
-        isRainy={true}
-      />
-    </div>
-  );
+function RainOrShine(props) {
+    const isRainy = props.isRainy;
+    if(isRainy) {
+        return(
+            <Rainy/>
+        );
+    }
+    return(
+        <h1>No rain today!</h1>
+    );
+}
+
+export default function DDX (){
+    return(
+        <div>
+            <RainOrShine isRainy={true}/>
+        </div>
+    );
 }
